a62f4c37016b19dada5f2a71e85f00d54c7af8ad
Daniele Marchei <danielemarchei@gmail.com>
2025-04-15 17:26:22 +0200

    version bump

--- frontend/lib/utils.dart
+++ frontend/lib/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
-String currentVersion = "4 Novembre 2024";
+String currentVersion = "15 Aprile 2025";
 
 double widthConstraint = 1080;
 double halfWidthConstraint = widthConstraint / 2;
